EIGHTFINITE/pokeemerald-expansion

AI `ShouldPivot` Overhaul (#8486)

include/battle_ai_util.h
@@ -141,7 +141,7 @@ bool32 ShouldRecover(u32 battlerAtk, u32 battlerDef, u32 move, u32 healPercent);
 bool32 ShouldSetScreen(u32 battlerAtk, u32 battlerDef, enum BattleMoveEffects moveEffect);
 bool32 ShouldCureStatus(u32 battlerAtk, u32 battlerDef, struct AiLogicData *aiData);
 bool32 ShouldCureStatusWithItem(u32 battlerAtk, u32 battlerDef, struct AiLogicData *aiData);
-enum AIPivot ShouldPivot(u32 battlerAtk, u32 battlerDef, enum Ability defAbility, u32 move, u32 moveIndex);
+enum AIPivot ShouldPivot(u32 battlerAtk, u32 battlerDef, u32 move);
 bool32 IsRecycleEncouragedItem(u32 item);
 bool32 ShouldRestoreHpBerry(u32 battlerAtk, u32 item);
 bool32 IsStatBoostingBerry(u32 item);
@@ -160,6 +160,7 @@ bool32 ShouldConsiderSelfSacrificeDamageEffect(u32 battlerAtk, u32 battlerDef, e
 
 // stat stage checks
 bool32 AnyStatIsRaised(u32 battlerId);
+bool32 AnyUsefulStatIsRaised(u32 battlerId);
 bool32 CanLowerStat(u32 battlerAtk, u32 battlerDef, struct AiLogicData *aiData, enum Stat stat);
 bool32 BattlerStatCanRise(u32 battler, enum Ability battlerAbility, enum Stat stat);
 bool32 AreBattlersStatsMaxed(u32 battler);

KrishanPatel001/Another-Pokeemerald-Hack

include/battle_ai_util.h
@@ -140,12 +140,12 @@ bool32 ShouldRecover(u32 battlerAtk, u32 battlerDef, enum Move move, u32 healPer
 bool32 ShouldSetScreen(u32 battlerAtk, u32 battlerDef, enum BattleMoveEffects moveEffect);
 bool32 ShouldCureStatus(u32 battlerAtk, u32 battlerDef, struct AiLogicData *aiData);
 bool32 ShouldCureStatusWithItem(u32 battlerAtk, u32 battlerDef, struct AiLogicData *aiData);
-enum AIPivot ShouldPivot(u32 battlerAtk, u32 battlerDef, enum Move move);
-bool32 IsRecycleEncouragedItem(enum Item item);
-bool32 ShouldRestoreHpBerry(u32 battlerAtk, enum Item item);
-bool32 IsStatBoostingBerry(enum Item item);
-bool32 CanKnockOffItem(u32 fromBattler, u32 battler, enum Item item);
-bool32 IsAbilityOfRating(enum Ability ability, s32 rating);
+enum AIPivot ShouldPivot(u32 battlerAtk, u32 battlerDef, u32 move);
+bool32 IsRecycleEncouragedItem(u32 item);
+bool32 ShouldRestoreHpBerry(u32 battlerAtk, u32 item);
+bool32 IsStatBoostingBerry(u32 item);
+bool32 CanKnockOffItem(u32 battler, u32 item);
+bool32 IsAbilityOfRating(enum Ability ability, s8 rating);
 bool32 AI_IsAbilityOnSide(u32 battlerId, enum Ability ability);
 bool32 AI_MoveMakesContact(u32 battlerAtk, u32 battlerDef, enum Ability ability, enum HoldEffect holdEffect, enum Move move);
 bool32 IsUnseenFistContactMove(u32 battlerAtk, u32 battlerDef, enum Move move);